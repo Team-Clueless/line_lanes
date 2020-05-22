a043bfd883a2909715ac15974b26a302e943a089
Suraj <suraj.rathi00@gmail.com>
2020-05-22 13:07:03 +0530

    Commented and explained the logic in lanes

--- lanes/lanes_mono.cpp
+++ lanes/lanes_mono.cpp
@@ -10,36 +10,43 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
 
-#include <dynamic_reconfigure/server.h>
-#include <igvc_bot/LanesConfig.h>
-
 #include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
 
+// Run `rosrun rqt_reconfigure rqt_reconfigure` go to /camera/lanes to change params for this node.
+#include <dynamic_reconfigure/server.h>
+#include <igvc_bot/LanesConfig.h>
 
+
+// Objects that the callback needs. Initialized in main().
 struct Helpers {
     ros::Publisher pub_point_cloud;
     image_transport::Publisher pub_masked;
 
-    // Params:
-    cv::Scalar white_lower, white_upper;
+    cv::Scalar white_lower, white_upper; // HSV range for color white.
+
+    // For cv::erode
     uint8_t erosion_size, erosion_iter;
     cv::Mat erosion_element;
-    bool publish_masked;
+
+    // for cv::blur
     uint8_t blur_size;
 
-    image_geometry::PinholeCameraModel cameraModel; // TODO change to stereo
+    bool publish_masked;
+
+    // For projecting the image onto the ground.
+    image_geometry::PinholeCameraModel cameraModel;
     float height;
 };
 
+
 const char *topic_image = "image_rect_color";
 const char *topic_camera_info = "camera_info";
-
 const char *topic_masked = "image_masked";
-
 const char *topic_pointcloud2 = "points2";
 
 const char *ground_frame = "ground";
+
 
 void callback(const sensor_msgs::ImageConstPtr &msg_left,
               Helpers &helper) {
@@ -49,28 +56,34 @@ void callback(const sensor_msgs::ImageConstPtr &msg_left,
 
         cv_bridge::CvImageConstPtr cv_img = cv_bridge::toCvCopy(msg_left, "");
 
-        cv::cvtColor(cv_img->image, hsv, cv::COLOR_BGR2HSV);
 
+        cv::cvtColor(cv_img->image, hsv, cv::COLOR_BGR2HSV);
         cv::GaussianBlur(hsv, blur, cv::Size(helper.blur_size, helper.blur_size), 0, 0);
 
+        // Get white pixels
         cv::inRange(blur, helper.white_lower, helper.white_upper, raw_mask);
 
-        // Remove Sky
+        // Flood Fill from the top of the mask to remove the sky in gazebo.
         cv::floodFill(raw_mask, cv::Point(raw_mask.cols / 2, 2), cv::Scalar(0));
 
         cv::erode(raw_mask, eroded_mask, helper.erosion_element, cv::Point(-1, -1), helper.erosion_iter);
 
+        // TODO: Make sure we arent detecting any weird blobs and only the lane.
+
         cv_img->image.copyTo(masked, eroded_mask);
 
-        // TODO: Remove more circular blobs. (the lane is an extremely elliptical blob)
 
         if (helper.publish_masked)
             helper.pub_masked.publish(
                     cv_bridge::CvImage(cv_img->header, cv_img->encoding, masked).toImageMsg());
 
-        std::vector<cv::Point> points;
+
+        std::vector<cv::Point> points; // All the points which is detected as part of the lane.
         cv::findNonZero(eroded_mask, points);
 
+
+        // Initialize the point cloud:
+        // See: https://answers.ros.org/question/212383/transformpointcloud-without-pcl/
         sensor_msgs::PointCloud2Ptr point_cloud = boost::make_shared<sensor_msgs::PointCloud2>();
         point_cloud->header.frame_id = helper.cameraModel.tfFrame();
         point_cloud->header.stamp = ros::Time::now();
@@ -79,27 +92,42 @@ void callback(const sensor_msgs::ImageConstPtr &msg_left,
         point_cloud->is_bigendian = false;
         point_cloud->is_dense = false;
         sensor_msgs::PointCloud2Modifier pc_mod(*point_cloud);
-        pc_mod.setPointCloud2FieldsByString(1, "xyz");
+
+        pc_mod.setPointCloud2FieldsByString(1, "xyz"); // Only want to publish spatial data.
         sensor_msgs::PointCloud2Iterator<float> x(*point_cloud, "x"), y(*point_cloud, "y"), z(*point_cloud, "z");
 
 
-        // TODO : Deal with the robot being tilted !!!
         for (const auto &point : points) {
-            cv::Point3d ray = helper.cameraModel.projectPixelTo3dRay(point); // Ray is a diff camera frame
+            // Ray is a vector that points from the camera to the pixel:
+            // Its calculation is pretty simple but is easier to use the image_geometry package.
 
-            //            cv::Point3d correct_ray(ray.z, -ray.x, -ray.y);
-            //
-            //            correct_ray *= -helper.height / correct_ray.z;
-            //            *x = correct_ray.x;
-            //            *y = correct_ray.y;
-            //            *z = correct_ray.z;
-            if (!ray.y) continue;
+            cv::Point3d ray = helper.cameraModel.projectPixelTo3dRay(point);
+            /* Note: the ray is not in the same frame as the tf_frame: camera_left
+             * Ray frame:
+             * x points to the right of the image, y points down, z points inward
+             *
+             * camera_left frame:
+             * x points in the image and y points up
+             *
+             * If you look at the camera from above:
+             *
+             * ray: x <---(x) y  camera_left: z (.)---> y
+             *             |                     |
+             *             |                     |
+             *             z                     x
+             *
+             * What we do is basically scale the ray such that the end touches the ground (we know the lane points are actually on the ground.
+             * Then we add those co-ords to the point cloud.
+            */
+
+            if (!ray.y) continue; // For divide by zero (ray.y != 0)
 
             ray *= helper.height / ray.y;
             *x = ray.z;
             *y = -ray.x;
-            *z = -ray.y;
+            *z = -ray.y; // Basically ground_height *z = helper.height
 
+            // Go to next point.
             ++x;
             ++y;
             ++z;
@@ -107,13 +135,15 @@ void callback(const sensor_msgs::ImageConstPtr &msg_left,
 
         helper.pub_point_cloud.publish(point_cloud);
 
-
     } catch (std::exception &e) {
         ROS_ERROR("Callback failed: %s", e.what());
     }
 }
 
-
+// This allows us to change params of the node while it is running: see cfg/lanes.cfg.
+// Try running `rosrun rqt_reconfigure rqt_reconfigure` while node is running.
+// This also auto loads any params initially set in the param server.
+// The the ros page for 'dynamic_reconfigure'
 void dynamic_reconfigure_callback(const igvc_bot::LanesConfig &config, const uint32_t &level, Helpers &helpers) {
     if (level & 1u << 0u) {
         ROS_INFO("Reconfiguring lower level.");
@@ -146,32 +176,39 @@ void dynamic_reconfigure_callback(const igvc_bot::LanesConfig &config, const uin
     }
 }
 
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "lane");
     ros::NodeHandle nh;
 
     ROS_INFO("Started");
 
+    // For receiving and publishing the images in an easy way.
     image_transport::ImageTransport imageTransport(nh);
 
     Helpers helper{
             nh.advertise<sensor_msgs::PointCloud2>(topic_pointcloud2, 5),
             imageTransport.advertise(topic_masked, 1),
 
-            (0, 0, 0), // Initialize from dynamic directly?
+            (0, 0, 0),
             (180, 40, 255),
+
             2,
             1,
             cv::getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE, cv::Size(2 * 3 + 1, 2 * 3 + 1)),
-            false,
-            7
+
+            7,
+
+            false
     };
+
 
     sensor_msgs::CameraInfo::ConstPtr camera_info = ros::topic::waitForMessage<sensor_msgs::CameraInfo>(
             topic_camera_info);
     helper.cameraModel.fromCameraInfo(camera_info);
 
-    {
+
+    { // For getting the height.
         tf::TransformListener listener;
         while (true) {
             try {
@@ -186,13 +223,14 @@ int main(int argc, char **argv) {
         }
     }
 
-
+    // For the dynamic parameter reconfigeration. see the function dynamic_reconfigure_callback
     dynamic_reconfigure::Server<igvc_bot::LanesConfig> server;
     dynamic_reconfigure::Server<igvc_bot::LanesConfig>::CallbackType dynamic_reconfigure_callback_function = boost::bind(
             &dynamic_reconfigure_callback, _1, _2, boost::ref(helper));
     server.setCallback(dynamic_reconfigure_callback_function);
 
 
+    // Adds the callback
     image_transport::Subscriber sub = imageTransport.subscribe(topic_image, 2,
                                                                boost::bind(&callback, _1, boost::ref(helper)));
 
--- lanes/lanes_stereo.cpp
+++ lanes/lanes_stereo.cpp
@@ -1,4 +1,5 @@
-//  NOT READY
+//  This is a small module that runs a callback with the latest images from both cameras.
+// Currently does nothing.
 
 #include <ros/ros.h>
 
@@ -7,104 +8,38 @@
 #include <image_transport/image_transport.h>
 #include <image_transport/subscriber_filter.h>
 
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/point_cloud2_iterator.h>
-
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
 #include <cv_bridge/cv_bridge.h>
 
-#include <image_geometry/pinhole_camera_model.h>
-
 #include <opencv/cv.h>
-
-#include <dynamic_reconfigure/server.h>
-#include <igvc_bot/LanesConfig.h>
 
 #include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
 
+// Bunch of objects that are useful in the callback
 struct Helpers {
-    ros::Publisher pub_point_cloud;
+    sensor_msgs::CameraInfo::ConstPtr camera_info;
+
     image_transport::Publisher pub_right, pub_left;
 
-    // Params:
-    cv::Scalar white_lower, white_upper;
-    uint8_t erosion_size, erosion_iter;
-    cv::Mat erosion_element;
-
-    image_geometry::PinholeCameraModel model_left; // TODO change to stereo
     float height;
 };
 
 const char *topic_right = "right/image_rect_color";
 const char *topic_left = "left/image_rect_color";
-const char *topic_left_camera = "left/camera_info";
+const char *topic_camera_info = "left/camera_info";
 
 const char *pub_topic_right = "right/image_masked";
 const char *pub_topic_left = "left/image_masked";
 
 
-const char *points_topic = "points2";
-
-
 void callback(const sensor_msgs::ImageConstPtr &msg_right, const sensor_msgs::ImageConstPtr &msg_left,
               Helpers &helper) {
-
     try {
-        cv::Mat hsv_left, mask_left, masked_left, mask_left_eroded;
-
-        cv_bridge::CvImagePtr cv_img_left = cv_bridge::toCvCopy(msg_left, "bgr8");
-
-        cv::cvtColor(cv_img_left->image, hsv_left, cv::COLOR_BGR2HSV);
-
-        cv::GaussianBlur(hsv_left, hsv_left, cv::Size(7, 7), 0, 0);
-
-        cv::inRange(hsv_left, helper.white_lower, helper.white_upper, mask_left);
-        cv::floodFill(mask_left, cv::Point(mask_left.cols / 2, 2), cv::Scalar(0));
-        cv::erode(mask_left, mask_left_eroded, helper.erosion_element, cv::Point(-1, -1), helper.erosion_iter);
-
-        cv_img_left->image.copyTo(masked_left, mask_left_eroded);
-
-        // TODO: Remove more circular blobs. (the lane is an extremely elliptical blob)
-
-        helper.pub_left.publish(
-                cv_bridge::CvImage(cv_img_left->header, cv_img_left->encoding, masked_left).toImageMsg());
-
-        std::vector<cv::Point> points;
-        cv::findNonZero(mask_left_eroded, points);
-
-        sensor_msgs::PointCloud2Ptr point_cloud = boost::make_shared<sensor_msgs::PointCloud2>();
-        point_cloud->header.frame_id = helper.model_left.tfFrame().c_str();
-        point_cloud->header.stamp = ros::Time::now();
-        point_cloud->height = 1;
-        point_cloud->width = points.size();
-        point_cloud->is_bigendian = false;
-        point_cloud->is_dense = false;
-        sensor_msgs::PointCloud2Modifier pc_mod(*point_cloud);
-        pc_mod.setPointCloud2FieldsByString(1, "xyz");
-        sensor_msgs::PointCloud2Iterator<float> x(*point_cloud, "x"), y(*point_cloud, "y"), z(*point_cloud, "z");
-
-
-        // TODO : Deal with the robot being tilted !!!
-        for (const auto &point : points) {
-            cv::Point3d ray = helper.model_left.projectPixelTo3dRay(point);
-            cv::Point3d correct_ray(ray.z, -ray.x, -ray.y);
-
-            correct_ray *= -helper.height / correct_ray.z;
-            *x = correct_ray.x;
-            *y = correct_ray.y;
-            *z = correct_ray.z;
-
-
-            ++x;
-            ++y;
-            ++z;
-        }
-
-        helper.pub_point_cloud.publish(point_cloud);
-
+        cv_bridge::CvImagePtr cv_img_left = cv_bridge::toCvCopy(msg_left, "bgr8"), cv_img_right = cv_bridge::toCvCopy(
+                msg_right, "bgr8");
 
     } catch (std::exception &e) {
         ROS_ERROR("Callback failed: %s", e.what());
@@ -112,60 +47,32 @@ void callback(const sensor_msgs::ImageConstPtr &msg_right, const sensor_msgs::Im
 }
 
 
-void dynamic_reconfigure_callback(const igvc_bot::LanesConfig &config, const uint32_t &level, Helpers &helpers) {
-    ROS_INFO("Reconfiguring...");
-    if (level & 1u << 0u) {
-        ROS_INFO("Reconfiguring lower level.");
-        helpers.white_lower = cv::Scalar(config.h_lower, config.s_lower, config.v_lower);
-    }
-
-    if (level & 1u << 1u) {
-        ROS_INFO("Reconfiguring upper level.");
-        helpers.white_upper = cv::Scalar(config.h_upper, config.s_upper, config.v_upper);
-    }
-
-    if (level & 1u << 2u) {
-        ROS_INFO("Reconfiguring erosion kernel.");
-        helpers.erosion_size = config.erosion_size;
-        helpers.erosion_iter = config.erosion_iter;
-        helpers.erosion_element = cv::getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE,
-                                                            cv::Size(2 * helpers.erosion_size + 1,
-                                                                     2 * helpers.erosion_size + 1));
-    }
-}
-
 int main(int argc, char **argv) {
     ros::init(argc, argv, "lane");
     ros::NodeHandle nh;
 
     ROS_INFO("Started");
 
+    // Create both the subscribers:
     image_transport::ImageTransport imageTransport(nh);
     image_transport::SubscriberFilter right(imageTransport, topic_right, 4),
             left(imageTransport, topic_left, 4);
 
+
+    // Create the sync thing
     message_filters::Synchronizer<message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image>>
             filter(message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image>(5),
                    right, left);
 
-    sensor_msgs::CameraInfo::ConstPtr camera_info_left = ros::topic::waitForMessage<sensor_msgs::CameraInfo>(
-            topic_left_camera);
 
     Helpers helper{
-            nh.advertise<sensor_msgs::PointCloud2>(points_topic, 5),
+            ros::topic::waitForMessage<sensor_msgs::CameraInfo>(topic_camera_info)
             imageTransport.advertise(pub_topic_right, 1),
             imageTransport.advertise(pub_topic_left, 1),
-
-            // Params:
-            (0, 0, 0), // Initialize from dynamic directly?
-            (180, 40, 255),
-            2,
-            1,
-            cv::getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE, cv::Size(2 * 3 + 1, 2 * 3 + 1))
     };
 
-    helper.model_left.fromCameraInfo(camera_info_left);
 
+    // Get the height from the ground.
     {
         tf::TransformListener listener;
         while (true) {
@@ -182,12 +89,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    dynamic_reconfigure::Server<igvc_bot::LanesConfig> server;
-    dynamic_reconfigure::Server<igvc_bot::LanesConfig>::CallbackType dynamic_reconfigure_callback_function = boost::bind(
-            &dynamic_reconfigure_callback, _1, _2, boost::ref(helper));
-    server.setCallback(dynamic_reconfigure_callback_function);
-
-
+    // Adds the callback
     const message_filters::Connection conn = filter.registerCallback(
             boost::bind(&callback, _1, _2, boost::ref(helper)));
 
