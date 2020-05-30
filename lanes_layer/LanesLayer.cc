3d8c467b1cc30918d6140166b7ea100ce9094f96
Suraj <suraj.rathi00@gmail.com>
2020-05-31 02:37:50 +0530

    Started adding the costmap layer. mixed some params.

---
+++ lanes_layer/LanesLayer.cc
@@ -0,0 +1,45 @@
+#include "igvc_bot/LanesLayer.h"
+
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(lanes_layer::LanesLayer, costmap_2d::Layer)
+
+using costmap_2d::LETHAL_OBSTACLE;
+
+namespace lanes_layer {
+
+    LanesLayer::LanesLayer() : dsrv_(0) {}
+
+    LanesLayer::~LanesLayer() {
+        delete dsrv_;
+    }
+
+    void LanesLayer::onInitialize() {
+        ros::NodeHandle nh("~/" + name_);
+        current_ = true;
+
+        dsrv_ = new dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>(nh);
+        dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig>::CallbackType cb = boost::bind(
+                &LanesLayer::reconfigureCB, this, _1, _2);
+        dsrv_->setCallback(cb);
+    }
+
+
+    void LanesLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level) {
+        enabled_ = config.enabled;
+    }
+
+    void LanesLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double *min_x,
+                                  double *min_y, double *max_x, double *max_y) {
+        if (!enabled_)
+            return;
+
+    }
+
+    void LanesLayer::updateCosts(costmap_2d::Costmap2D &master_grid, int min_i, int min_j, int max_i,
+                                 int max_j) {
+        if (!enabled_)
+            return;
+    }
+
+} // end namespace
