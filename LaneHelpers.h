3454bd536e45e6aa29663c3b824fa1d1473dace8
Suraj <suraj.rathi00@gmail.com>
2020-06-03 12:08:52 +0530

    Comments and cleanup!

--- LaneHelpers.h
+++ LaneHelpers.h
@@ -50,6 +50,7 @@ public:
     double operator()(std::pair<double, double> point) const;
 };
 
+// GETS ABSOLUTE VALUE
 class vert_dist {
     // For a line passing through prev and cur.
 
@@ -60,7 +61,46 @@ class vert_dist {
 public:
     vert_dist(std::pair<double, double> cur, std::pair<double, double> prev);
 
+    // GETS ABSOLUTE VALUE
     double operator()(std::pair<double, double> point) const;
+};
+
+#include <image_transport/image_transport.h>
+#include <opencv/cv.h>
+#include <tf/transform_listener.h>
+#include <image_geometry/pinhole_camera_model.h>
+
+struct Helpers {
+    struct Publishers {
+        image_transport::Publisher masked;
+        PCPublisher pc;
+        LanePublisher lane;
+    } pub;
+
+    struct CVParams {
+        cv::Scalar white_lower, white_upper, barrel_lower, barrel_upper; // HSV range for color white.
+
+        double rect_frac;
+
+        uint8_t erosion_size, erosion_iter;
+        cv::Mat erosion_element;
+
+        // for cv::blur
+        uint8_t blur_size;
+    } cv;
+
+    bool publish_masked;
+
+    struct LanesParams {
+        size_t max_points, stride;
+        double epsilon_dist, max_vert_dist;
+        double max_horiz_dist, min_new_dist, max_new_dist;
+        double cos_max_new_angle, cos_min_angle_reverse_pt;
+    } lanes;
+
+    // For projecting the image onto the ground.
+    image_geometry::PinholeCameraModel cameraModel;
+    tf::TransformListener listener;
 };
 
 
