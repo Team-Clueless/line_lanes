a8f310329aeaa39e46f1cfea8c8bb34b008a8e96
Suraj <suraj.rathi00@gmail.com>
2020-06-05 10:22:11 +0530

    Now reads all values as parameters for setup

--- LaneHelpers.h
+++ LaneHelpers.h
@@ -6,10 +6,23 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
 #include <mutex>
+#include <utility>
+#include <opencv/cv.h>
 
 template<typename T>
-T getParam(const std::string &key, T default_value);
+T getParam(const std::string &key, T default_value) {
+    T val;
+    if (ros::param::get(key, val))
+        return val;
+    return default_value;
+}
 
+cv::Scalar getCvScalar(std::vector<double> v);
+
+/*
+template int getParam<int>;
+template double getParam<double>;
+*/
 
 class PCPublisher {
     ros::Publisher _pub;
@@ -90,24 +103,52 @@ struct Helpers {
         uint8_t erosion_size, erosion_iter;
         cv::Mat erosion_element;
 
-        // for cv::blur
         uint8_t blur_size;
-    } cv;
 
-    bool publish_masked;
+        bool publish_masked;
+    } cv{
+            getCvScalar(getParam<std::vector<double> >("cv/lane_lower", {0, 0, 0})),
+            getCvScalar(getParam<std::vector<double> >("cv/lane_upper", {180, 40, 255})),
+
+            getCvScalar(getParam<std::vector<double> >("cv/barrel_lower", {0, 250, 0})),
+            getCvScalar(getParam<std::vector<double> >("cv/barrel_upper", {180, 255, 255})),
+
+            (getParam("cv/upper_mask_percent", 60) % 100) / 100.0,
+
+            (uint8_t) getParam("cv/erosion_size", 2),
+            (uint8_t) getParam("cv/erosion_iter", 1),
+            cv::getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE, cv::Size(2 * 3 + 1, 2 * 3 + 1)),
+
+            (uint8_t) getParam("cv/blur_size", 7),
+            getParam("cv/publish_masked", true)
+    };
 
     struct LanesParams {
         size_t max_points, stride;
         double epsilon_dist, max_vert_dist;
         double max_horiz_dist, min_new_dist, max_new_dist;
         double cos_max_new_angle, cos_min_angle_reverse_pt;
-    } lanes;
+    } lanes{
+            (size_t) getParam("lanes/max_points", 10000),
+            (size_t) getParam("lanes/stride", 20),
+            getParam("lanes/epsilon_dist", 0.25),
+            getParam("lanes/max_vert_dist", 0.74),
+            getParam("lanes/max_horiz_dist", 4.5),
+            getParam("lanes/min_new_dist", 0.5),
+            getParam("lanes/max_new_dist", 4.0),
+            getParam("lanes/cos_max_new_angle", 0.8),
+            getParam("lanes/cos_min_angle_reverse_point", 0.2)
+    };
 
     // For projecting the image onto the ground.
     image_geometry::PinholeCameraModel cameraModel;
     tf::TransformListener listener;
 
+    bool dynamic_reconfigure{getParam("dynamic_reconfigure", false)};
+
     std::mutex mutex;
+
+    Helpers(Publishers pubs) : pub(std::move(pubs)), mutex() {}
 };
 
 #endif //IGVC_BOT_LANEHELPERS_H
